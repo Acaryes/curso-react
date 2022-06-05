9a7b92150ca504508e61ad73b38a3a24c1f90276
Acaryes <acaryes@gmail.com>
2022-06-05 16:39:38 -0700

    added NavBar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import "./NavBar.css";
+
+export default function NavBar() {
+
+    return(
+
+        <div class="topnav" id="myTopnav">
+          <a href="#home" class="active">Inicio</a>
+          <a href="#productos">Productos</a>
+          <a href="#contacto">Contacto</a>
+          <a href="#sobrenosotros">Sobre Nosotros</a>
+        </div>
+
+    );
+}
